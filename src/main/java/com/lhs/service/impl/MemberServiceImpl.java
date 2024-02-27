6ec92b295c35cf5087ae178240b80fffa971e86f
WonseokDevKim <dnjstjr2012@gmail.com>
2024-02-27 14:32:41 +0900

    회원가입 구현

---
+++ src/main/java/com/lhs/service/impl/MemberServiceImpl.java
@@ -0,0 +1,60 @@
+package com.lhs.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lhs.dao.MemberDao;
+import com.lhs.exception.PasswordMissMatchException;
+import com.lhs.exception.UserNotFoundException;
+import com.lhs.service.MemberService;
+import com.lhs.util.Sha512Encoder;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+	
+	@Autowired MemberDao mDao; 
+
+	@Override
+	public ArrayList<HashMap<String, Object>> memberList(HashMap<String, Object> params) {
+		return mDao.memberList(params);
+	}
+	
+	//총회원수
+	@Override
+	public int totalMemberCnt(HashMap<String, Object> params) {
+		return mDao.totalMemberCnt(params);
+	}
+
+	@Override
+	public int join(HashMap<String, String> params) {
+		// 사용자가 입력한 비밀번호(평문) 암호화
+		String passwd = params.get("memberPw");
+		String encodeTxt = Sha512Encoder.getInstance().getSecurePassword(passwd);
+		params.put("memberPw", encodeTxt);
+		return mDao.join(params);
+	}
+
+	@Override
+	public int checkId(HashMap<String, String> params) {
+		return mDao.checkId(params);
+	}
+
+	@Override
+	public HashMap<String, Object> login(HashMap<String, String> params) throws UserNotFoundException, PasswordMissMatchException {
+		HashMap<String, Object> member = mDao.getMemberById(params);
+		return member;
+	}
+
+	@Override
+	public int delMember(HashMap<String, Object> params) {	
+		return mDao.delMember(params);
+	}
+
+
+
+	
+
+}
